e8a46604e2b6a8f6cdb8eaaf9a2e756e3a065fa7
Tushar Raj Choudhary <132886519+Tushar0326@users.noreply.github.com>
2024-08-01 20:54:20 +0530

    Add files via upload

---
+++ Major Project -1/musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "Tamil Song":"https://www.youtube.com/watch?v=a3Ue-LN5B9U&pp=ygUPYWFzYSBrb29kYSBzb25n",
+    "Paper before money":"https://www.youtube.com/watch?v=edwpDfxgEqY&pp=ygUWbmF2YWFuIHNhbmRodSBuZXcgc29uZw%3D%3D",
+    "Addicted": "https://www.youtube.com/watch?v=uxy254BGsxM&pp=ygUWbmF2YWFuIHNhbmRodSBuZXcgc29uZw%3D%3D",
+}
